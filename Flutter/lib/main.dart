d35620f8fdfbe06e80bc4ceff7692a78b5b62b69
Lundhede <m010795@hotmail.com>
2019-04-16 12:48:20 +0200

    Split af filer

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'sign_in.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,75 +16,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class RecommenderState extends State<Recommender> {
-  
-  Widget _logInScreen(){
-    return Scaffold(
-      backgroundColor: Color(0xFFCAF8F3),
-      body: Center(
-        child: Column(
-          children: <Widget>[  
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-            ),
-            MaterialButton(
-              minWidth: 170,
-              height: 50,
-              onPressed: () {_logInScreen();},
-              child: const Text('Facebook', style: TextStyle(color: Colors.white,)),
-              color: Color(0xFF3C5A99),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-            ),
-            MaterialButton(
-              minWidth: 170,
-              height: 50,
-              onPressed: () {_logInScreen();},
-              child: const Text('Twitter', style: TextStyle(color: Colors.white,)),
-              color: Color(0xFF1DA1F2),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-            ),
-            MaterialButton(
-              minWidth: 170,
-              height: 50,
-              onPressed: () {_logInScreen();},
-              child: const Text('Google'),
-              color: Color(0xFFFFFFFF),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-            ),
-            MaterialButton(
-              minWidth: 170,
-              height: 50,
-              onPressed: () {_logInScreen();},
-              child: const Text('New User'),
-              color: Color(0xFFFFFDD0),            
-            ),
-          ]
-        ),
-      ),
-    );
-  }
-  
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign in'),        
-      ),
-      body: _logInScreen(),
-    );
-  }
-}
-
-class Recommender extends StatefulWidget {
-  @override
-  RecommenderState createState() => RecommenderState();
-}
-
