3475954e6c15b2aed6be893c12e7d0aa6d18ba9d
Lundhede <m010795@hotmail.com>
2019-05-17 18:42:36 +0200

    Pre merge III

--- Flutter/lib/utility.dart
+++ Flutter/lib/utility.dart
@@ -349,7 +349,7 @@ Future<List<Attraction>> getRecommendations(
       return recAttractions;
 
     } else {
-      print('No connection to server: GR');
+      displayMsg('No connection to server: \nGR', context);
     }
   } catch (e) {
     print('gr: ');
